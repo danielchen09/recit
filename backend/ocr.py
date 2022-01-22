f4a2f6628ae067593e4931f6d42a3c7ed7eab679
danielchen09 <chen3189@purdue.edu>
2022-01-22 03:42:44 -0500

    #add ocr implementation

---
+++ backend/ocr.py
@@ -0,0 +1,99 @@
+import os
+import re
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath('api_key/recit-339006-22c537cac291.json')
+
+MIN_TEXT_LEN = 2
+MAX_TEXT_LEN = 20
+SPLIT_THRESHOLD = 40
+is_price = re.compile('^[$-]?[0-9]+\\.[0-9][0-9]$')
+
+with open('exclude_words.txt', 'r') as f:
+    EXCLUDE_WORDS = f.read().split('\n')
+
+
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    bounds = []
+    for text in texts:
+        if len(text.description) < MIN_TEXT_LEN or len(text.description) > MAX_TEXT_LEN:
+            continue
+        vertices = ([(vertex.x, vertex.y) for vertex in text.bounding_poly.vertices])
+        bounds.append({
+            'text': re.sub('[^a-zA-z0-9.]', '', text.description),
+            'center': ((vertices[0][0] + vertices[3][0]) / 2, (vertices[1][1] + vertices[2][1]) / 2)
+        })
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+    return bounds
+
+
+def generate_item(raw_data):
+    price = -1
+    name = []
+    for d in raw_data:
+        if is_price.match(d['text']):
+            price = float(re.sub('[^0-9.]', '', d['text']))
+        else:
+            name.append(d['text'])
+    if price == -1:
+        return None
+    for word in name:
+        if word.lower() in EXCLUDE_WORDS:
+            return None
+    name = ' '.join(name)
+    if name.lower() in EXCLUDE_WORDS:
+        return None
+    if 'total' in name.lower():
+        name = 'TOTAL'
+    return {
+        'name': name,
+        'price': price
+    }
+
+# bounds = detect_text('imgs/237880506_319402293264065_3950863099988236330_n.jpg')
+# bounds = detect_text('imgs/239456377_1167790013743760_5376371282799389019_n.jpg')
+# bounds = detect_text('imgs/240400627_953078555241461_2086325353596572994_n.jpg')
+# bounds = detect_text('imgs/271444539_1092758391567580_5328614794881049298_n.jpg')
+# bounds = detect_text('imgs/271584576_376473660949125_4045385816272921141_n.jpg')
+# bounds = detect_text('imgs/271686130_652217512692928_2807764537634750669_n.jpg')
+# bounds = detect_text('imgs/271732175_920970361951537_4667307938951366310_n.jpg')
+# bounds = detect_text('imgs/271740618_318562186705622_3723177510778391907_n.jpg')
+
+
+def parse_receipt(path):
+    bounds = sorted(detect_text(path), key=lambda x: x['center'][1])
+    processing = []
+    items = {}
+    for i in range(1, len(bounds)):
+        if bounds[i]['center'][1] - bounds[i - 1]['center'][1] > SPLIT_THRESHOLD:
+            processing = sorted(processing, key=lambda x: x['center'][0])
+            item = generate_item(processing)
+            if item:
+                if item['name'] not in items:
+                    items[item['name']] = {
+                        'price': item['price'],
+                        'qty': 0
+                    }
+                items[item['name']]['qty'] += 1
+            processing = []
+        processing.append(bounds[i])
+
+    return items
